3806b5e6259a0110d58d39281fe95b016a039793
Jaroslav Bušek <jaroslav.busek@gmail.com>
2023-03-03 13:21:30 +0100

    Create lcd_displej_s_tlacitkem_pomoci_funkci.ino

---
+++ vyuka/LCD displej/lcd_displej_s_tlacitkem_pomoci_funkci/lcd_displej_s_tlacitkem_pomoci_funkci.ino
@@ -0,0 +1,55 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define PIN_TL  4
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+byte cislo;
+unsigned long cas_predchozi;
+int tlacitko_predchozi;
+
+void setup() {
+  init_displej();
+  init_tlacitko()
+  cas_predchozi = millis();
+}
+
+void loop() {
+  unsigned long cas = millis();
+
+  int stav = tlacitko();
+
+  if (stav) cislo = 0;
+
+  if (cas - cas_predchozi >= 1000) {
+    cislo++;
+    displej();
+    cas_predchozi = cas;
+  }
+}
+
+void tlacitko() {
+  int tlacitko = digitalRead(PIN_TL);
+  if (tlacitko == LOW && tlacitko_predchozi == HIGH) {
+    return 1;
+  } else return 0;
+  tlacitko_predchozi = tlacitko;
+}
+
+void init_tlacitko() {
+  pinMode(PIN_TL, INPUT_PULLUP);
+  tlacitko_predchozi = digitalRead(PIN_TL);
+}
+
+void init_displej() {
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+}
+
+void displej() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(cislo);
+}
