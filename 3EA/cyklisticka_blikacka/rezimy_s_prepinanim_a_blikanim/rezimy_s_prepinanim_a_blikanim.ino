cc93c3ac26357977190779296af87b80e40868a3
Jaroslav Bušek <nemotora@users.noreply.github.com>
2023-11-03 11:37:44 +0100

    Create rezimy_s_prepinanim_a_blikanim.ino

---
+++ 3EA/cyklisticka_blikacka/rezimy_s_prepinanim_a_blikanim/rezimy_s_prepinanim_a_blikanim.ino
@@ -0,0 +1,60 @@
+#define PIN_BTN 2
+#define PIN_LED 5
+#define T       1000
+
+int tik = LOW;
+
+void tikani();
+
+void setup() {
+  pinMode(PIN_BTN, INPUT_PULLUP);
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  tikani();
+  static int rezim = 0;
+  static int stav_tlacitka_predchozi = HIGH;
+  static int stav_led = LOW;
+  int stav_tlacitka = digitalRead(PIN_BTN);
+  if (stav_tlacitka == LOW && stav_tlacitka_predchozi == HIGH) {
+    if (rezim < 4) rezim++;  // rezim += 1;
+    else rezim = 0;
+    Serial.println(rezim);
+    //rezim = (rezim < 4) ? rezim++ : 0;
+  }
+  stav_tlacitka_predchozi = stav_tlacitka;
+
+  switch (rezim) {
+    case 0:  // LED nesvítí
+      stav_led = LOW;
+      break;
+    case 1:  // LED svítí souvisle
+      stav_led = HIGH;
+      break;
+    case 2:  // LED bliká pomalu
+      if (tik) stav_led = !stav_led;
+      break;
+    case 3:  // LED bliká rychle
+
+      break;
+    case 4:  // LED mění plynule intenzitu
+
+      break;
+    default:  // neznámý režim
+
+      break;
+  }
+  digitalWrite(PIN_LED, stav_led);
+}
+
+void tikani() {
+  unsigned long cas = millis();
+  static unsigned long cas_predtim = 0;
+  if (cas - cas_predtim >= T) {
+    // zmen stav LED
+    tik = HIGH;
+    cas_predtim = cas;
+  } else tik = LOW;
+}
