14e3a2cc6e1253fb7bb621fb2142cfcdecc51fd9
Jaroslav Bušek <nemotora@users.noreply.github.com>
2023-11-15 08:08:08 +0100

    Update init.ino

--- 3EA/init/init.ino
+++ 3EA/init/init.ino
@@ -1,9 +1,18 @@
+#define PIN_TLACITKO  2
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  pinMode(PIN_TLACITKO, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-
-
+  int stav = !digitalRead(PIN_TLACITKO);
+  if (stav) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
