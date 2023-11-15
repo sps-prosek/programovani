92137fc6a6763a29e51bc82b353b3c540c1f1de8
Jaroslav Bušek <nemotora@users.noreply.github.com>
2023-11-15 08:08:02 +0100

    Create rezimy.ino

---
+++ 3EA/cyklisticka_blikacka/rezimy/rezimy.ino
@@ -0,0 +1,27 @@
+void setup() {
+
+}
+
+void loop() {
+  static int rezim = 0;
+  switch (rezim) {
+    case 0: // LED nesvítí
+
+      break;
+    case 1: // LED svítí souvisle
+
+      break;
+    case 2: // LED bliká pomalu
+
+      break;
+    case 3: // LED bliká rychle
+
+      break;
+    case 4: // LED mění plynule intenzitu
+
+      break;
+    default:  // neznámý režim
+
+    break;
+  }
+}
