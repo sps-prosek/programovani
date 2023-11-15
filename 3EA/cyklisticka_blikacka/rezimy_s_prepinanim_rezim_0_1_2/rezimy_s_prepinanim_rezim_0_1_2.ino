6f47611f547a715cb1650a57bc38bac32ff7d174
Jaroslav Bušek <nemotora@users.noreply.github.com>
2023-11-15 09:28:06 +0100

    Update rezimy_s_prepinanim_rezim_0_1_2.ino

--- 3EA/cyklisticka_blikacka/rezimy_s_prepinanim_rezim_0_1_2/rezimy_s_prepinanim_rezim_0_1_2.ino
+++ 3EA/cyklisticka_blikacka/rezimy_s_prepinanim_rezim_0_1_2/rezimy_s_prepinanim_rezim_0_1_2.ino
@@ -1,6 +1,8 @@
 #define PIN_BTN 2
 #define PIN_LED 5
-#define T       1000
+#define T 1000
+
+int tik = LOW;
 
 void setup() {
   pinMode(PIN_BTN, INPUT_PULLUP);
@@ -9,6 +11,10 @@ void setup() {
 }
 
 void loop() {
+
+  tikani();
+
+  static int stav_led = LOW;
   static int rezim = 0;
   static int stav_tlacitka_predchozi = HIGH;
   int stav_tlacitka = digitalRead(PIN_BTN);
@@ -20,8 +26,6 @@ void loop() {
   }
   stav_tlacitka_predchozi = stav_tlacitka;
 
-
-  
   switch (rezim) {
     case 0:  // LED nesvítí
       digitalWrite(PIN_LED, LOW);
@@ -30,7 +34,8 @@ void loop() {
       digitalWrite(PIN_LED, HIGH);
       break;
     case 2:  // LED bliká pomalu
-
+      if (tik) stav_led = !stav_led;
+      digitalWrite(PIN_LED, stav_led);
       break;
     case 3:  // LED bliká rychle
 
@@ -42,4 +47,13 @@ void loop() {
 
       break;
   }
+}
+
+void tikani() {
+  unsigned long cas = millis();
+  static unsigned long cas_predtim = 0;
+  if (cas - cas_predtim >= T) {
+    tik = HIGH;
+    cas_predtim = cas;
+  } else tik = LOW;
 }
